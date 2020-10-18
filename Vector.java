ff19078022f1aebd832345474258bcffec8820ff
Dan Hales <danhalesprogramming@gmail.com>
2020-10-18 16:59:59 -0400

    added more methods

--- Vector.java
+++ Vector.java
@@ -1,8 +1,13 @@
 package linearalgebra;
 
 /**
- * The Vector class provides basic vector operations for 
- * arrays of real numbers. All operations assume that vectors 
+ * The Vector class provides basic vector operations for Euclidean vectors
+ * represented as arrays of real numbers. 
+ *
+ * All operations between two vectors are designed for vectors of the same
+ * length, and no checking is done. For loops are controlled by the length
+ * of the first vector, so if the second vector is longer, an Exception may
+ * not be thrown as expected.
  */
  
 public class Vector {
@@ -33,6 +38,79 @@ public class Vector {
    }
    
    /**
+    * angleDegrees method computes the angle between the two vectors, 
+    * computed as arccos(u1.dot(u2) / (u1.magnitude() * u2.magnitude())
+    * @param u1 a Vector object
+    * @param u2 a Vector object
+    * @return the angle between u1 and u2 (in radians)
+    */
+    
+   public static double angleDegrees(Vector u1, Vector u2) {
+      return Vector.angleRadians(u1, u2) * 180 / Math.PI;
+   }
+   
+   /**
+    * angleRadians method computes the angle between the two vectors, 
+    * computed as arccos(u1.dot(u2) / (u1.magnitude() * u2.magnitude())
+    * @param u1 a Vector object
+    * @param u2 a Vector object
+    * @return the angle between u1 and u2 (in radians)
+    */
+   public static double angleRadians(Vector u1, Vector u2) {
+      return Math.acos(Vector.dotProduct(u1, u2) / (u1.magnitude() * u2.magnitude()));
+   }
+   
+   /**
+    * dot method computes the dot product of the calling vector and
+    * the passed vectored.
+    * assumes vectors have the same length.
+    * @param u a Vector object
+    * @return the sum of the products of corresponding elements
+    */
+   public double dot(Vector u) {
+      return Vector.dotProduct(this, u);
+   }
+   
+   /**
+    * dotProduct method computes the dot product of two vectors.
+    * assumes vectors have the same length.
+    * @param u1 a Vector object
+    * @param u2 a Vector object
+    * @return the sum of the products of corresponding elements
+    */
+   public static double dotProduct(Vector u1, Vector u2) {
+      double sum = 0;
+      
+      for (int i = 0; i < u1.length(); i++) {
+         sum += (u1.get(i) * u2.get(i));
+      }
+      
+      return sum;
+   }
+   
+   /**
+    * isZero checks to see if all entries are zero.
+    * @return true if all entries in v are zero, false otherwise
+    */
+   public boolean isZero() {
+      for (double entry : v) {
+         if (entry != 0) { // if a non-zero entry is found
+            return false;
+         }
+      }
+      
+      return true;
+   }
+   
+   /**
+    * magnitude method is a wrapper for normL2
+    * @return the magnitude of the vector
+    */
+   public double magnitude() {
+      return Vector.normL2(this);
+   }
+   
+   /**
     * multiply method accepts a scalar to and multiplies each entry
     * in v by it.
     * @param u the vector to add onto the calling vector.
@@ -42,6 +120,36 @@ public class Vector {
    public Vector multiply(double scalar) {
       return Vector.product(this, scalar);
    }
+   
+   /**
+    * normalize scales the passed vector by dividing it by its 
+    * magnitude. if the zero vector is passed, an IllegalArgumentException 
+    * is thrown.
+    * @param u a Vector object
+    * @return a Vector object
+    */
+   public static Vector normalize(Vector v) {
+      if (v.isZero()) {
+         throw new IllegalArgumentException();
+      } else {
+         return v.multiply(1.0/v.magnitude());
+      }
+   }
+   
+   /**
+    * normalize scales the calling vector by dividing it by its 
+    * magnitude. if the zero vector is passed, an IllegalArgumentException 
+    * is thrown.
+    * @return a Vector object
+    */
+   public Vector normalize() {
+      if (this.isZero()) {
+         throw new IllegalArgumentException();
+      } else {
+         return this.multiply(1.0/this.magnitude());
+      }
+   }   
+   
    
    /**
     * normL1 accepts a Vector and returns the L1 norm (the sum
@@ -80,7 +188,7 @@ public class Vector {
          sum += Math.pow(u.get(i), 2);
       }
       
-      return sum;
+      return Math.sqrt(sum);
    }
    
    /**
@@ -95,15 +203,15 @@ public class Vector {
     * product accepts a Vector object and a scalar and returns
     * a Vector whose entries are the entries of the Vector, multiplied
     * by the scalar.
-    * @param vector a Vector object
+    * @param u a Vector object
     * @param scalar a real number
     * @return the scalar product of the vector and the scalar
     */
-   public static Vector product(Vector vector, double scalar) {
-      double[] products = new double[vector.length()];
+   public static Vector product(Vector u, double scalar) {
+      double[] products = new double[u.length()];
       
       for (int i = 0; i < products.length; i++) {
-         products[i] = scalar * vector.get(i);
+         products[i] = scalar * u.get(i);
       }
       
       return new Vector(products);
@@ -113,8 +221,8 @@ public class Vector {
     * sum method accepts two vectors and returns their element-wise
     * sum in a new Vector object. Assumes v1 and v2 have the same 
     * length.
-    * @param v1 a Vector object
-    * @param v2 a Vector object
+    * @param u1 a Vector object
+    * @param u2 a Vector object
     */
    public static Vector sum(Vector u1, Vector u2) {
       double[] sums = new double[u1.length()];
@@ -186,11 +294,9 @@ public class Vector {
          
          if (i < (this.v.length - 1)) { // if we're not at the last entry
             str += sep;
-         } else {
-            str += "]";
          }
       }
       
-      return str;
+      return str + "]";
    }
 }
